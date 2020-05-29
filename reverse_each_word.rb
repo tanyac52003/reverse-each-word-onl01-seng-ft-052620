d0900a742645d1f4a0924114856d663cd75550f9
Tanya Hodges <tanyac52003@gmail.com>
2020-05-29 16:50:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do |string|
-    return_array << string.reverse 
-  end
-  return_array.join(" ")
-end
+ array = string.split(" ")
+ new_array = []
+ array.collect do |string|
+   test_array << string.reverse 
+ end
+ test_array.join(" ")
+end 
 
